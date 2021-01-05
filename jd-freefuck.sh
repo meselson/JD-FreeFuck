65832ad2750e4efff66e281cd6de9cdc60f6de8a
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-05 19:15:59 +0000

    Update jd-freefuck.sh

--- jd-freefuck.sh
+++ jd-freefuck.sh
@@ -27,11 +27,11 @@ wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos
 yum makecache > /dev/null 2>&1
 yum install -y https://mirrors.aliyun.com/epel/epel-release-latest-8.noarch.rpm
 yum makecache
-yum install -y net-tools curl git perl nodejs npm
+yum install -y net-tools curl git perl nodejs npm moreutils
 yum install -y http://rpmfind.net/linux/centos/8.3.2011/PowerTools/x86_64/os/Packages/perl-IO-Tty-1.12-11.el8.x86_64.rpm
 yum install -y http://rpmfind.net/linux/centos/8.3.2011/PowerTools/x86_64/os/Packages/perl-IPC-Run-0.99-1.el8.noarch.rpm
-yum install -y moreutils
-yum update -y
+#yum install -y moreutils
+#yum update -y
 #语言环境判定：
 locale | grep 'LANG=zh_CN.UTF-8' -q
 if [ $? -eq 0 ];then
