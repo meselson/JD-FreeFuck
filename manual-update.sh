ce25b1d91bcf0c1e7cc308e50e91467dace422e3
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-12 03:19:41 +0800

    Update manual-update.sh

--- manual-update.sh
+++ manual-update.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-touch /home/myid/jd/manual-update.sh
-chmod +x /home/myid/jd/manual-update.sh
-cat > /home/myid/jd/manual-update.sh << EOF
-#!/bin/bash
 bash git_pull.sh
 rm -rf run-all.sh
 touch run-all.sh
 chmod +x run-all.sh
-bash jd.sh | grep jd_ >> run-all.sh
+bash jd.sh | grep _ >> run-all.sh
 sed -i '1d' run-all.sh
 sed -i 's/^/bash jd.sh &/g' run-all.sh
 sed -i 's/$/& now/g' run-all.sh
-echo "bash jd.sh jx_sign now"
 sed -i '1i\#!/bin/bash' run-all.sh
 EOF
