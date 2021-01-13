eccb8d2a41b6c7e72386f1ad65f62fca345cb7da
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-13 00:15:19 +0000

    Update manual-update.sh

--- manual-update.sh
+++ manual-update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+rm -rf /home/myid/jd/manual-update.sh
+touch /home/myid/jd/manual-update.sh
+chmod +x /home/myid/jd/manual-update.sh
+cat > /home/myid/jd/manual-update.sh << EOF
+#!/bin/bash
 bash git_pull.sh
 rm -rf run-all.sh
 touch run-all.sh
@@ -8,3 +13,5 @@ sed -i '1d' run-all.sh
 sed -i 's/^/bash jd.sh &/g' run-all.sh
 sed -i 's/$/& now/g' run-all.sh
 sed -i '1i\#!/bin/bash' run-all.sh
+EOF
+echo -e "\033[32m ------------- 更新成功 ------------- \033[0m"
