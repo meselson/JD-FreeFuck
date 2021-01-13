8efcfd62382e0e410ee4ab6da7ddb690820671dc
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-14 04:46:58 +0800

    一键更新脚本

--- manual-update.sh
+++ manual-update.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 rm -rf /home/myid/jd/manual-update.sh
 touch /home/myid/jd/manual-update.sh
-chmod +x /home/myid/jd/manual-update.sh
 cat > /home/myid/jd/manual-update.sh << EOF
 #!/bin/bash
 bash git_pull.sh
 rm -rf run-all.sh
 touch run-all.sh
-chmod +x run-all.sh
 bash jd.sh | grep _ >> run-all.sh
 sed -i '1d' run-all.sh
 sed -i 's/^/bash jd.sh &/g' run-all.sh
