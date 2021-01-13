8ae959cbc3e1665aac40a2ddc255dede7318afd4
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-13 20:14:26 +0800

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -13,24 +13,24 @@ COOKIE5='""'
 COOKIE6='""'
 
 JudgeUser(){
-    if [ $UID -ne 0 ];then
-        echo -e '\033[31m ------------ Permission no enough, please use user ROOT! ------------ \033[0m'
-        return
-    fi
+  if [ $UID -ne 0 ]; then
+    echo -e '\033[31m ------------ Permission no enough, please use user ROOT! ------------ \033[0m'
+    return
+  fi
 }
 
 JudgeNetwork(){
-    ping -c 1 www.baidu.com > /dev/null 2>&1
-    if [ $? -ne 0 ];then
-        echo -e "\033[31m ----- Network connection error.Please check the network environment and try again later! ----- \033[0m"
-        return
-    fi
+  ping -c 1 www.baidu.com >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo -e "\033[31m ----- Network connection error.Please check the network environment and try again later! ----- \033[0m"
+    return
+  fi
 }
 
 #环境部署：
 EnvDeploy(){
-    sed -i '1,$d' /etc/apt/sources.list
-    cat > /etc/apt/sources.list <<EOF
+  sed -i '1,$d' /etc/apt/sources.list
+  cat >/etc/apt/sources.list <<EOF
 deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
 deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
@@ -42,48 +42,48 @@ deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted univers
 deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 EOF
-    apt-get update
-    apt install -y git curl wget nodejs npm perl moreutils
+  apt-get update
+  apt install -y git curl wget nodejs npm perl moreutils
 }
 
 #项目部署：
 ScriptInstall(){
-    git clone -b v3 https://gitee.com/evine/jd-base /home/myid/jd
-    cd /home/myid/jd
-    mkdir config
-    cp sample/config.sh.sample config/config.sh && cp sample/computer.list.sample config/crontab.list
-    bash git_pull.sh
-    cd /home/myid/jd/scripts
-    npm install || npm install --registry=https://registry.npm.taobao.org
-    cd /home/myid/jd
+  git clone -b v3 https://gitee.com/evine/jd-base /home/myid/jd
+  cd /home/myid/jd
+  mkdir config
+  cp sample/config.sh.sample config/config.sh && cp sample/computer.list.sample config/crontab.list
+  bash git_pull.sh
+  cd /home/myid/jd/scripts
+  npm install || npm install --registry=https://registry.npm.taobao.org
+  cd /home/myid/jd
 }
 
 #更改配置文件：
 SetProfile(){
-    sed -i "27c Cookie1=$COOKIE1" config/config.sh
-    sed -i "28c Cookie2=$COOKIE2" config/config.sh
-    sed -i "29c Cookie3=$COOKIE3" config/config.sh
-    sed -i "30c Cookie4=$COOKIE4" config/config.sh
-    sed -i "31c Cookie5=$COOKIE5" config/config.sh
-    sed -i "32c Cookie6=$COOKIE6" config/config.sh
+  sed -i "27c Cookie1=$COOKIE1" config/config.sh
+  sed -i "28c Cookie2=$COOKIE2" config/config.sh
+  sed -i "29c Cookie3=$COOKIE3" config/config.sh
+  sed -i "30c Cookie4=$COOKIE4" config/config.sh
+  sed -i "31c Cookie5=$COOKIE5" config/config.sh
+  sed -i "32c Cookie6=$COOKIE6" config/config.sh
 }
 
 #编写一键执行脚本：
 AutoRun(){
-    touch /home/myid/jd/run-all.sh
-    chmod +x /home/myid/jd/run-all.sh
-    bash jd.sh | grep _ >> /home/myid/jd/run-all.sh
-    sed -i '1d' /home/myid/jd/run-all.sh
-    sed -i 's/^/bash jd.sh &/g' /home/myid/jd/run-all.sh
-    sed -i 's/$/& now/g' /home/myid/jd/run-all.sh
-    sed -i '1i\#!/bin/bash' /home/myid/jd/run-all.sh
+  touch /home/myid/jd/run-all.sh
+  chmod +x /home/myid/jd/run-all.sh
+  bash jd.sh | grep _ >>/home/myid/jd/run-all.sh
+  sed -i '1d' /home/myid/jd/run-all.sh
+  sed -i 's/^/bash jd.sh &/g' /home/myid/jd/run-all.sh
+  sed -i 's/$/& now/g' /home/myid/jd/run-all.sh
+  sed -i '1i\#!/bin/bash' /home/myid/jd/run-all.sh
 }
 
 #编写一键更新脚本：
-AutoUpdate(){
-    touch /home/myid/jd/manual-update.sh
-    chmod +x /home/myid/jd/manual-update.sh
-    cat > /home/myid/jd/manual-update.sh << EOF
+ManualUpdate(){
+  touch /home/myid/jd/manual-update.sh
+  chmod +x /home/myid/jd/manual-update.sh
+  cat >/home/myid/jd/manual-update.sh <<EOF
 #!/bin/bash
 bash git_pull.sh
 rm -rf run-all.sh
@@ -97,24 +97,27 @@ sed -i '1i\#!/bin/bash' run-all.sh
 EOF
 }
 
+#结束语：
+Tips(){
+  echo -e "\033[32m ------------------- 环境部署完成，请执行 bash run-all.sh 命令开始你的薅羊毛行为 ------------------- \033[0m"
+  echo -e "\033[32m +=================================================================================================+ \033[0m"
+  echo -e "\033[32m | 注意：该项目主运行目录为/home/myid/jd                                                           | \033[0m"
+  echo -e "\033[32m | 注意：为了保证脚本的正常运行，请不要更改任何组件的位置以避免出现未知的错误                      | \033[0m"
+  echo -e "\033[32m | 注意：执行脚本期间可能会卡住或运行挂机脚本，可通过命令 Ctrl + Z 跳过继续执行剩余活动脚本        | \033[0m"
+  echo -e "\033[32m | 注意：run-all.sh为执行所有活动脚本，仍可通过原作者 bash jd.sh 命令查看教程并执行特定活动脚本    | \033[0m"
+  echo -e "\033[32m | 注意：由于京东活动一直变化所以会出现无法参加活动等正常现象，可手动更新JavaScript活动脚本        | \033[0m"
+  echo -e "\033[32m | 注意：如果需要更新核心JavaScript活动脚本，请执行 bash manual-update.sh 命令进行一键更新即可     | \033[0m"
+  echo -e "\033[32m | 注意：之前填入的Cookie部分内容具有一定的时效性，若提示失效请根据教程重新获取并通过命令手动更新  | \033[0m"
+  echo -e "\033[32m | 定义：run-all.sh 为本人编写的一键执行所有活动脚本，manual-update.sh 为本人编写的一键更新脚本    | \033[0m"
+  echo -e "\033[32m +=================================================================================================+ \033[0m"
+  echo -e "\033[32m -------------------- 更多帮助请访问 https://github.com/SuperManito/JD-FreeFuck -------------------- \033[0m"
+}
+
 JudgeUser
 JudgeNetwork
 EnvDeploy
 ScriptInstall
 SetProfile
 AutoRun
-AutoUpdate
-
-#结束语：
-echo -e "\033[32m ------------------- 环境部署完成，请执行 bash run-all.sh 命令开始你的薅羊毛行为 ------------------- \033[0m"
-echo -e "\033[32m +=================================================================================================+ \033[0m"
-echo -e "\033[32m | 注意：该项目主运行目录为/home/myid/jd                                                           | \033[0m"
-echo -e "\033[32m | 注意：为了保证脚本的正常运行，请不要更改任何组件的位置以避免出现未知的错误                      | \033[0m"
-echo -e "\033[32m | 注意：执行脚本期间可能会卡住或运行挂机脚本，可通过命令 Ctrl + Z 跳过继续执行剩余活动脚本        | \033[0m"
-echo -e "\033[32m | 注意：run-all.sh为执行所有活动脚本，仍可通过原作者 bash jd.sh 命令查看教程并执行特定活动脚本    | \033[0m"
-echo -e "\033[32m | 注意：由于京东活动一直变化所以会出现无法参加活动等正常现象，可手动更新JavaScript活动脚本        | \033[0m"
-echo -e "\033[32m | 注意：如果需要更新核心JavaScript活动脚本，请执行 bash manual-update.sh 命令进行一键更新即可     | \033[0m"
-echo -e "\033[32m | 注意：之前填入的Cookie部分内容具有一定的时效性，若提示失效请根据教程重新获取并通过命令手动更新  | \033[0m"
-echo -e "\033[32m | 定义：run-all.sh 为本人编写的一键执行所有活动脚本，manual-update.sh 为本人编写的一键更新脚本    | \033[0m"
-echo -e "\033[32m +=================================================================================================+ \033[0m"
-echo -e "\033[32m -------------------- 更多帮助请访问 https://github.com/SuperManito/JD-FreeFuck -------------------- \033[0m"
+ManualUpdate
+Tips
