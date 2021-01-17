0a34cfe8873567dafc7672f739078b692e68c105
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-17 14:26:31 +0000

    CentOS一键部署脚本

--- install-centos.sh
+++ install-centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Author:SuperManito
-#Update Date:2021-1-17
+#Update Date:2021-1-13
 #Project Name:《京东薅羊毛》一键部署脚本 For Linux，通过参与京东商城的各种活动白嫖京豆
 #适用系统：CentOS 8简体中文，不适用7及更低版本，本人测试环境为最新CentOS 8.3，如果是最小化安装CentOS，请通过SSL方式进入到终端
 
@@ -14,90 +14,105 @@ COOKIE6='""'
 
 #当前用户判定：
 JudgeUser() {
-  if [ $UID -ne 0 ];then
-      echo -e '\033[31m ------------ Permission no enough, please use user ROOT! ------------ \033[0m'
-      return
-  fi
+if [ $UID -ne 0 ]; then
+  echo -e '\033[31m ------------ Permission no enough, please use user ROOT! ------------ \033[0m'
+  return
+fi
 }
 
 #网络环境判定：
 JudgeNetwork() {
-  ping -c 1 www.baidu.com > /dev/null 2>&1
-  if [ $? -ne 0 ];then
-      echo -e "\033[31m ----- Network connection error.Please check the network environment and try again later! ----- \033[0m"
-      return
-  fi
+ping -c 1 www.baidu.com >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo -e "\033[31m ----- Network connection error.Please check the network environment and try again later! ----- \033[0m"
+  return
+fi
 }
 
 #环境部署：
 EnvDeploy() {
-  systemctl disable --now firewalld
-  sed -i "7c SELINUX=disabled" /etc/selinux/config
-  setenforce 0
-  yum -y install wget
-  rm -rf /etc/yum.repos.d/*
+systemctl disable --now firewalld
+sed -i "7c SELINUX=disabled" /etc/selinux/config
+setenforce 0
+yum install -y wget
+rm -rf /etc/yum.repos.d/*
+VERSION=`cat /etc/redhat-release|sed -r "s/.* ([0-9]+)\..*/\1/"`
+if [ $VERSION -eq 7 ];then
+  wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+  yum install -y https://mirrors.aliyun.com/epel/epel-release-latest-7.noarch.rpm
+  yum makecache
+  yum install -y net-tools git curl perl moreutils gcc gcc-c++
+  wget https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/v14.14.0/node-v14.14.0-linux-x64.tar.gz
+  tar -zxvf node-v14.14.0-linux-x64.tar.gz -C /opt
+  mv /opt/node-v14.14.0-linux-x64 /opt/nodejs
+  ln -s /opt/nodejs/bin/node /usr/local/bin/
+  ln -s /opt/nodejs/bin/npm /usr/local/bin/
+  rm -rf node-v14.14.0-linux-x64.tar.gz
+fi
+if [ $VERSION -eq 8 ];then
   wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo
   yum makecache > /dev/null 2>&1
   yum install -y https://mirrors.aliyun.com/epel/epel-release-latest-8.noarch.rpm
   yum makecache
   yum install -y https://mirrors.aliyun.com/centos/8.3.2011/PowerTools/x86_64/os/Packages/perl-IO-Tty-1.12-11.el8.x86_64.rpm
   yum install -y https://mirrors.aliyun.com/centos/8.3.2011/PowerTools/x86_64/os/Packages/perl-IPC-Run-0.99-1.el8.noarch.rpm
-  yum install -y net-tools git curl wget nodejs npm perl moreutils
+  yum install -y net-tools git curl nodejs npm perl moreutils
+fi
 }
 
 #语言环境判定：
 JudgeLocale() {
-  locale | grep 'LANG=zh_CN' -q
-  if [ $? -eq 0 ];then
-      echo "\033[32m ------------ 语言环境正确，开始部署项目核心环境 ------------ \033[0m"
-      sleep 2s
-  else
-      yum -y install langpacks-zh_CN
-      locale -a
-      echo 'LANG="zh_CN.utf8"' > /etc/locale.conf
-      . /etc/locale.conf
-      type locale
-      echo -e "\033[33m -------- Please reboot your system and try again，Because the locale was updated. -------- \033[0m"
-      return
-  fi
+locale | grep 'LANG=zh_CN' -q
+if [ $? -eq 0 ];then
+  echo "\033[32m ------------ 语言环境正确，开始部署项目核心环境 ------------ \033[0m"
+  sleep 2s
+else
+  yum -y install langpacks-zh_CN
+  locale -a
+  echo 'LANG="zh_CN.utf8"' > /etc/locale.conf
+  . /etc/locale.conf
+  type locale
+  echo -e "\033[33m -------- Please reboot your system and try again，Because the locale was updated. -------- \033[0m"
+  return
+fi
 }
 
-#部署项目环境：
+#项目部署：
 ScriptInstall() {
-  git clone -b v3 https://gitee.com/evine/jd-base /home/myid/jd
-  cd /home/myid/jd
-  mkdir config
-  cp sample/config.sh.sample config/config.sh && cp sample/computer.list.sample config/crontab.list
-  bash git_pull.sh
-  cd /home/myid/jd/scripts
-  npm install || npm install --registry=https://registry.npm.taobao.org
-  cd /home/myid/jd
+git clone -b v3 https://gitee.com/evine/jd-base /home/myid/jd
+cd /home/myid/jd
+mkdir config
+cp sample/config.sh.sample config/config.sh && cp sample/computer.list.sample config/crontab.list
+bash git_pull.sh
+cd /home/myid/jd/scripts
+npm install || npm install --registry=https://registry.npm.taobao.org
+cd /home/myid/jd
 }
 
 #更改配置文件：
 CookieConfig() {
-  sed -i "27c Cookie1=$COOKIE1" config/config.sh
-  sed -i "28c Cookie2=$COOKIE2" config/config.sh
-  sed -i "29c Cookie3=$COOKIE3" config/config.sh
-  sed -i "30c Cookie4=$COOKIE4" config/config.sh
-  sed -i "31c Cookie5=$COOKIE5" config/config.sh
-  sed -i "32c Cookie6=$COOKIE6" config/config.sh
+sed -i "27c Cookie1=$COOKIE1" config/config.sh
+sed -i "28c Cookie2=$COOKIE2" config/config.sh
+sed -i "29c Cookie3=$COOKIE3" config/config.sh
+sed -i "30c Cookie4=$COOKIE4" config/config.sh
+sed -i "31c Cookie5=$COOKIE5" config/config.sh
+sed -i "32c Cookie6=$COOKIE6" config/config.sh
 }
 
 #编写一键执行脚本：
 RunAll() {
-  touch /home/myid/jd/run-all.sh
-  bash jd.sh | grep _ >> /home/myid/jd/run-all.sh
-  sed -i '1d' /home/myid/jd/run-all.sh
-  sed -i 's/^/bash jd.sh &/g' /home/myid/jd/run-all.sh
-  sed -i 's/$/& now/g' /home/myid/jd/run-all.sh
-  sed -i '1i\#!/bin/bash' /home/myid/jd/run-all.sh
+touch /home/myid/jd/run-all.sh
+bash jd.sh | grep _ >>/home/myid/jd/run-all.sh
+sed -i '1d' /home/myid/jd/run-all.sh
+sed -i 's/^/bash jd.sh &/g' /home/myid/jd/run-all.sh
+sed -i 's/$/& now/g' /home/myid/jd/run-all.sh
+sed -i '1i\#!/bin/bash' /home/myid/jd/run-all.sh
 }
 
 #编写一键更新脚本：
 ManualUpdate() {
-  touch /home/myid/jd/manual-update.sh
-  cat > /home/myid/jd/manual-update.sh << EOF
+touch /home/myid/jd/manual-update.sh
+cat >/home/myid/jd/manual-update.sh <<EOF
 #!/bin/bash
 bash git_pull.sh
 rm -rf run-all.sh
