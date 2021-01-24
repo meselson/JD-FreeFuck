2d240375a18b8dacbc0dcf27e3c3f2aaf735e556
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-01-24 22:50:07 +0800

    一键更新脚本

--- manual-update.sh
+++ manual-update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Author:SuperManito
+#Update Date:2021-1-24
 rm -rf /home/myid/jd/manual-update.sh
 touch /home/myid/jd/manual-update.sh
 cat > /home/myid/jd/manual-update.sh << EOF
@@ -6,10 +8,10 @@ cat > /home/myid/jd/manual-update.sh << EOF
 bash git_pull.sh
 rm -rf run-all.sh
 touch run-all.sh
-bash jd.sh | grep _ >> run-all.sh
-sed -i '1d' run-all.sh
+bash jd.sh | grep -o 'jd_[a-z].*' > run-all.sh
+bash jd.sh | grep -o 'jx_[a-z].*' >> run-all.sh
 sed -i 's/^/bash jd.sh &/g' run-all.sh
 sed -i 's/$/& now/g' run-all.sh
-sed -i '1i\#!/bin/bash' run-all.sh
 EOF
+bash manual-update.sh
 echo -e "\033[32m ---------------------- 更新成功 ---------------------- \033[0m"
