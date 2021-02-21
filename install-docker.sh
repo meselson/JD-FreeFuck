0dfc084a7f02bc258aebcae490f1e9463e6c96c8
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-02-21 22:07:00 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -7,16 +7,18 @@ rm -rf ~/.ssh/id_rsa
 wget -P ~/.ssh https://gitee.com/SuperManito/JD-FreeFuck/raw/main/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-## 替换更新文件
+## 删除旧的文件
+rm -rf scripts
 rm -rf git_pull.sh
+rm -rf sample/config.sh.sample
+## 替换更新文件
 wget https://gitee.com/SuperManito/JD-FreeFuck/raw/main/git_pull.sh
 ## 替换配置文件模板
-rm -rf sample/config.sh.sample
 wget -P sample https://gitee.com/SuperManito/JD-FreeFuck/raw/main/config.sh.sample
 ## 创建配置文件
-sample/config.sh.sample config/config.sh
-sample/computer.list.sample config/crontab.list
+cp sample/config.sh.sample config/config.sh
+cp sample/computer.list.sample config/crontab.list
 ## 拉取项目文件
 bash git_pull.sh
 ## 更正定时任务
-sed -i "s#/home/myid/jd/jd.sh#jd#g" config/crontab.list
+sed -i "s#/home/myid/jd/jd.sh#jd.sh#g" config/crontab.list
