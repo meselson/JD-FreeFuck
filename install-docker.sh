86ff8f7559d183f880504d06f5dfdeb659e8805a
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-02-21 22:24:12 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -21,4 +21,32 @@ cp sample/computer.list.sample config/crontab.list
 ## 拉取项目文件
 bash git_pull.sh
 ## 更正定时任务
-sed -i "s#/home/myid/jd/jd.sh#jd.sh#g" config/crontab.list
+sed -i "s#/home/myid/jd/##g" config/crontab.list
+## 编写一键更新脚本
+touch $BASE/manual-update.sh
+cat >$BASE/manual-update.sh <<\EOF
+#!/bin/bash
+## Author:SuperManito
+## Modified:2021-2-21
+
+## 执行更新命令
+bash git_pull.sh
+## 重新生成一键执行所有活动脚本
+rm -rf run-all.sh
+touch run-all.sh
+bash jd.sh | grep -o 'jd_[a-z].*' >run-all.sh
+bash jd.sh | grep -o 'jx_[a-z].*' >>run-all.sh
+sed -i 's/^/bash jd.sh &/g' run-all.sh
+sed -i 's/.js/ now/g' run-all.sh
+sed -i '1i\#!/bin/bash' run-all.sh
+sed -i "s/bash jd.sh jd_delCoupon now//g" run-all.sh  #不执行京东家庭号任务
+sed -i "s/bash jd.sh jd_family now//g" run-all.sh     #不执行删除优惠券任务
+cat run-all.sh | grep jd_crazy_joy_coin -wq
+if [ $? -eq 0 ];then
+  sed -i "s/bash jd.sh jd_crazy_joy_coin now//g" run-all.sh
+  echo "bash jd.sh jd_crazy_joy_coin now" >>run-all.sh
+fi
+sed -i '/^\s*$/d' run-all.sh
+## 配置定时任务
+sed -i "s#/home/myid/jd/##g" config/crontab.list
+EOF
