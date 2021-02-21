a61d9bb02fb271b8f7f48c7d83bf7b655a764835
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-02-21 17:10:49 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -23,7 +23,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ScriptsURL=https://gitee.com/lxk0301/jd_scripts
+ScriptsURL=git@gitee.com:lxk0301/jd_scripts.git
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
 ## 每天次数随机，更新时间随机，更新秒数随机，至少6次，至多12次，大部分为8-10次，符合正态分布。
