892a203c35dfa0aa9f77dec581c3d7bf19c87c3c
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-02-22 22:16:53 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -11,17 +11,19 @@ ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 rm -rf scripts
 rm -rf git_pull.sh
 rm -rf sample/config.sh.sample
-## 替换更新文件
+## 下载新版更新文件
 wget https://gitee.com/SuperManito/JD-FreeFuck/raw/main/git_pull.sh
-## 替换配置文件模板
+## 下载新版配置文件模板
 wget -P sample https://gitee.com/SuperManito/JD-FreeFuck/raw/main/config.sh.sample
 ## 创建配置文件
-cp sample/config.sh.sample config/config.sh
-cp sample/computer.list.sample config/crontab.list
+rm -rf config.sh
+cp -f sample/config.sh.sample config/config.sh
+cp -f sample/computer.list.sample config/crontab.list
 ## 拉取项目文件
 bash git_pull.sh
 ## 更正定时任务
-sed -i "s#/home/myid##g" config/crontab.list
+sed -i "s#/home/myid/jd/jd.sh#jd#g" config/crontab.list
+sed -i "s#/home/myid/jd/##g" config/crontab.list
 ## 编写一键更新脚本
 touch manual-update.sh
 cat >manual-update.sh <<\EOF
@@ -48,6 +50,6 @@ if [ $? -eq 0 ];then
 fi
 sed -i '/^\s*$/d' run-all.sh
 ## 配置定时任务
-sed -i "s#/home/myid##g" config/crontab.list
+sed -i "s#/home/myid/jd/jd.sh#jd#g" config/crontab.list
 EOF
 bash manual-update.sh
