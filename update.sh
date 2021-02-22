21f5e950b9f53210215e721bb4632298e4385189
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-02-23 00:00:43 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,10 +3,40 @@
 ## Date:2021-2-22
 ## 更新说明：修复了Crontab定时配置错误的问题
 
-docker exec -it jd rm -rf manual-update.sh
-docker exec -it jd wget https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/main/manual-update.sh
-docker exec -it jd bash manual-update.sh
-docker exec -it jd sed -i "s#/home/myid/jd/##g" config/crontab.list
+## 编写新的一键更新脚本
+rm -rf manual-update.sh
+touch manual-update.sh
+cat >manual-update.sh <<\EOF
+#!/bin/bash
+## Author:SuperManito
+## Modified:2021-2-22
+
+## 执行更新命令
+bash git_pull.sh
+## 重新生成一键执行所有活动脚本
+rm -rf run-all.sh
+touch run-all.sh
+bash jd.sh | grep -o 'jd_[a-z].*' >run-all.sh
+bash jd.sh | grep -o 'jx_[a-z].*' >>run-all.sh
+sed -i 's/^/bash jd.sh &/g' run-all.sh
+sed -i 's/.js/ now/g' run-all.sh
+sed -i '1i\#!/bin/bash' run-all.sh
+sed -i "s/bash jd.sh jd_delCoupon now//g" run-all.sh  #不执行京东家庭号任务
+sed -i "s/bash jd.sh jd_family now//g" run-all.sh     #不执行删除优惠券任务
+cat run-all.sh | grep jd_crazy_joy_coin -wq
+if [ $? -eq 0 ];then
+  sed -i "s/bash jd.sh jd_crazy_joy_coin now//g" run-all.sh
+  echo "bash jd.sh jd_crazy_joy_coin now" >>run-all.sh
+fi
+sed -i '/^\s*$/d' run-all.sh
+## 配置定时任务
+sed -i "s#/home/myid/jd/jd.sh#jd#g" config/crontab.list
+sed -i "s/git_pull/manual-update/g" config/crontab.list
+EOF
+## 修改非活动脚本的定时配置
+sed -i "s#/home/myid/jd/##g" config/crontab.list
+## 更新活动脚本
+bash manual-update.sh
 
 echo -e "\033[32m +------------------------ 更 新 成 功 ------------------------+ \033[0m"
 echo -e "\033[32m |                                                             | \033[0m"
