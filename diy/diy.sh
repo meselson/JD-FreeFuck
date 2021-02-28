49206ab12de4cb8d3cbeb5efd446b78259ebc985
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-02-28 21:59:13 +0800

    删除了失效的活动

--- diy/diy.sh
+++ diy/diy.sh
@@ -29,7 +29,7 @@ scripts_base_url_1=https://raw.githubusercontent.com/i-chenzhe/qx/main/
 
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
 # 将相应作者的脚本填写到以下变量中
-my_scripts_list_1="jd_asus_iqiyi.js jd_entertainment.js jd_jump-jump.js"
+my_scripts_list_1="jd_entertainment.js jd_jump-jump.js"
 
 
 ## 活动脚本名称1：华硕-爱奇艺、百变大咖秀、母婴-跳一跳
@@ -85,7 +85,7 @@ done
 ##########################  删  除  旧  的  失  效  活  动  ##########################
 ## 删除旧版本失效的活动示例： rm -rf ${ShellDir}/scripts/jd_test.js >/dev/null 2>&1
 rm -rf ${ShellDir}/scripts/ddxw.js
-rm -rf ${ShellDir}/scripts/*anslove.js
+rm -rf ${ShellDir}/scripts/jd_asus_iqiyi.js
 rm -rf ${ShellDir}/scripts/jd_collectBlueCoin.js
 
 
@@ -93,7 +93,6 @@ rm -rf ${ShellDir}/scripts/jd_collectBlueCoin.js
 ## 注意两边修改内容区别在于中间内容"jd"、"${ShellDir}/jd.sh"
 ## 修正定时任务示例：sed -i "s|bash jd jd_test|bash ${ShellDir}/jd.sh test|g" config/crontab.list
 ##                 sed -i "s|bash jd jd_ceshi|bash ${ShellDir}/jd.sh ceshi|g" config/crontab.list
-sed -i "s|bash jd jd_asus_iqiyi|bash ${ShellDir}/jd.sh jd_asus_iqiyi|g" config/crontab.list
 sed -i "s|bash jd jd_entertainment|bash ${ShellDir}/jd.sh jd_entertainment|g" config/crontab.list
 sed -i "s|bash jd jd_jump-jump|bash ${ShellDir}/jd.sh jd_jump-jump|g" config/crontab.list
 
