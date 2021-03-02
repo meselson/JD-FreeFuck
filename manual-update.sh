6891fcb6832b9899f0c3c7fb747038d86e8603d2
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-03-02 22:29:37 +0800

    变更了一些活动的执行顺序

--- manual-update.sh
+++ manual-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## Author:SuperManito
-## Version 1.2
-## Modified:2021-2-28
+## Version 1.3
+## Modified:2021-3-02
 
 ## ================== 定 义 变 量 ==============================================
 ## 项目目录
@@ -12,7 +12,7 @@ Proxy_URL=https://ghproxy.com/
 
 ## ================== 定 义  d i y 脚 本 同 步 链 接 ============================
 ## diy 自定义脚本链接
-Diy_URL=https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/main/diy/diy.sh
+DIY_URL=https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/main/diy/diy.sh
 ## 此脚本用于执行非 lxk 的第三方脚本
 ## 默认同步本人项目中的 diy.sh 脚本
 ## 不建议您直接使用其它人的脚本，因为本项目中的 diy 脚本高度定制
@@ -29,10 +29,11 @@ bash $BASE/git_pull.sh
 ## 生成一键执行所有活动脚本
 ## 默认将 "jd、jx、jr" 开头的活动脚本加入其中
 rm -rf $BASE/run-all.sh
-bash $BASE/jd.sh | grep -o 'j[drx]_[a-z].*' >$BASE/run-all.sh
+bash $BASE/jd.sh | grep -o 'j[drx]_[a-z].*' | grep -v 'bean_change' >$BASE/run-all.sh
+sed -i "1i\jd_bean_change.js" $BASE/run-all.sh       ## 置顶京豆变动通知
 sed -i "s#^#bash $BASE/jd.sh &#g" $BASE/run-all.sh
 sed -i 's#.js# now#g' $BASE/run-all.sh
-sed -i '1i\#!/bin/bash' $BASE/run-all.sh
+sed -i '1i\#!/bin/env bash' $BASE/run-all.sh
 ## 自定义添加脚本
 ## 例：echo "bash $BASE/jd.sh xxx now" >>$BASE/run-all.sh
 
@@ -54,22 +55,28 @@ fi
 
 ## 去除不想加入到此脚本中的活动
 ## 例：sed -i '/xxx/d' $BASE/run-all.sh
-sed -i '/jd_delCoupon/d' $BASE/run-all.sh   ## 不执行 "京东家庭号" 任务
-sed -i '/jd_family/d' $BASE/run-all.sh      ## 不执行 "删除优惠券" 任务
+sed -i '/jd_code/d' $BASE/run-all.sh        ## 不输出所有活动列表
+sed -i '/jd_delCoupon/d' $BASE/run-all.sh   ## 不执行 "京东家庭号" 活动
+sed -i '/jd_family/d' $BASE/run-all.sh      ## 不执行 "删除优惠券" 活动
 
 
 ## 去除脚本中的空行
 sed -i '/^\s*$/d' $BASE/run-all.sh
+## 赋权
+chmod 777 $BASE/run-all.sh
 ################################################################################
 
 ## ================= 同 步 d i y 脚 本 功 能 ====================================
 ## 自动更新 Diy 脚本
 function DiyUpdate() {
-  echo -e "\033[37mdiy脚本同步开始... \033[0m"
-  echo -e ''
-  wget -q $Proxy_URL$Diy_URL -O $BASE/config/diy.sh
-  echo -e "\033[37mdiy脚本同步完成... \033[0m"
-  echo -e ''
+  wget -q $Proxy_URL$DIY_URL -O $BASE/config/diy.sh
+  if [ $? -eq 0 ];then
+    echo -e "\033[37mdiy脚本同步成功... \033[0m"
+    echo -e ''
+  else
+    echo -e "\033[37mdiy脚本同步失败... \033[0m"
+    echo -e ''
+  fi
 }
 #DiyUpdate   
 ## 默认关闭此功能，将 " #DiyUpdate " 取消注释修改为 " DiyUpdate " 即代表启用此功能
