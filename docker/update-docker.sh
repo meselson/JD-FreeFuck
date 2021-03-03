cf3527e9189e38d3199df33d15f4d7863222345d
SuperManito <68613938+SuperManito@users.noreply.github.com>
2021-03-04 00:59:41 +0800

    Update update-docker.sh

--- docker/update-docker.sh
+++ docker/update-docker.sh
@@ -1,6 +1,29 @@
-#!/bin/bash
+#!/bin/env bash
 ## Author:SuperManito
-## Date:2021-3-
+## Date:2021-3-4
+
+## 项目安装目录
+BASE="/jd"
+
+## 代理链接
+Proxy_URL=https://ghproxy.com/
+## 一键更新脚本地址
+Git_Pull_URL=https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/source/git_pull.sh
+## 配置文件模板地址
+Config_URL=https://raw.githubusercontent.com/SuperManito/JD-FreeFuck/source/sample/config.sh.sample
+
+
+## 删除旧的脚本
+rm -rf $BASE/manual-update.sh
+## 更新一键脚本
+wget $Proxy_URL$Git_Pull_URL -O $BASE/git_pull.sh
+## 更新配置文件模板
+wget $Proxy_URL$Config_URL -O $BASE/sample/config.sh.sample
+## 备份当前配置文件
+mv $BASE/config/config.sh $BASE/config/config.sh.bak
+echo -e "已备份当前使用配置文件至 $BASE/config/config.sh.bak ... "
+## 替换新的配置文件
+cp -f $BASE/sample/config.sh.sample $BASE/config/config.sh
 
 echo -e "\033[32m +------------------------ 更 新 成 功 ------------------------+ \033[0m"
 echo -e "\033[32m |                                                             | \033[0m"
